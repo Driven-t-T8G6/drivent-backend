3940b0b7ecb17085b315ccb73e62566800caa545
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2023-03-10 01:14:34 -0300

    feat: event is now save in redis

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import { createClient } from 'redis';
 
 export let prisma: PrismaClient;
-export function connectDb(): void {
+export const redisClient = createClient();
+
+export async function connectDb(): Promise<void> {
   prisma = new PrismaClient();
+  await redisClient.connect();
 }
 
 export async function disconnectDB(): Promise<void> {
   await prisma?.$disconnect();
+  await redisClient.quit();
 }
